a12b944018e294caf8b71a9bc84f7913e2ac62f1
Davit Mkrtumyan <141499493+dav777dav@users.noreply.github.com>
2025-07-17 19:35:55 +0400

    Add files via upload

---
+++ add_two_numbers.asm
@@ -0,0 +1,47 @@
+extern printf
+extern scanf
+
+section .data
+    text1 db "Enter first number: ", 0
+    text2 db "Enter second number: ", 0
+    result db "Sum: %d", 10, 0
+    scan db "%d", 0
+
+section .bss
+    num1 resd 1
+    num2 resd 1
+
+section .text
+    global main:
+main:
+
+    push rbp
+
+    mov rdi, text1
+    xor rax, rax
+    call printf
+
+    mov rdi, scan
+    mov rsi, num1
+    xor rax, rax
+    call scanf
+
+    mov rdi, text2
+    xor rax, rax
+    call printf
+
+    mov rdi, scan
+    mov rsi, num2
+    xor rax, rax
+    call scanf
+
+    mov eax, [num1]
+    add eax, [num2]
+
+    mov rdi, result
+    mov rsi, rax
+    xor rax, rax
+    call printf
+
+    pop rbp
+    ret
